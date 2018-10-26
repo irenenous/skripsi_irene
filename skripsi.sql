577ee5483937e911ef5d236f8844b2123d42eedb
irenenous <irenenous@yahoo.com>
2018-10-26 23:25:28 +0700

    remove sql comments

--- skripsi.sql
+++ skripsi.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Oct 26, 2018 at 05:52 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
